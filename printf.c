69930393d6a3556c78bc82a4f77e94edbe9645c4
natscott1738 <natebaraka777@gmail.com>
2023-10-18 03:49:20 -0700

    printf

--- printf.c
+++ printf.c
@@ -10,84 +10,87 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count;
-    va_start(args, format);
+	va_list args;
+	int count = 0;
 
-    count = 0;
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's' || *(format + 1) == '%' || *(format + 1) == 'd' || *(format + 1) == 'i'))
+		{
+			char c;
+			char *s;
+			int d;
 
-    while (*format) {
-        char c;
-        const char *str;
-        int num;
-	long lnum;
-        unsigned int unum;
-        void *ptr;
+			switch (*(format + 1))
+			{
+				case 'c':
+					c = (char)va_arg(args, int);
+					write(1, &c, 1);
+					count++;
+					break;
+				case 's':
+					s = va_arg(args, char *);
+					while (*s)
+					{
+						write(1, s, 1);
+						s++;
+						count++;
+					}
+					break;
+				case '%':
+					write(1, "%", 1);
+					count++;
+					break;
+				case 'd':
+				case 'i':
+					d = va_arg(args, int);
+					if (d < 0)
+					{
+						write(1, "-", 1);
+						count++;
+						d = -d;
+					}
+					char buffer[11];
+					int len = 0;
+					do
+					{
+						buffer[len++] = '0' + d % 10;
+						d /= 10;
+					}
+					while (len > 0)
+					{
+						write(1, &buffer[--len], 1);
+						count++;
+					}
+					break;
+			}
+			format += 2;
+		}
+		else
+		{
+			write(1, format, 1);
+			count++;
+			format++;
+		}
+	}
+	va_end(args);
+	return count;
+}
 
-        if (*format == '%') {
-            format++;
-            switch (*format) {
-                case 'c':
-                    c = va_arg(args, int);
-                    putchar(c);
-                    count++;
-                    break;
-                case 's':
-                    str = va_arg(args, const char *);
-                    while (*str) {
-                        putchar(*str);
-                        count++;
-                        str++;
-                    }
-                    break;
-                case '%':
-                    putchar('%');
-                    count++;
-                    break;
-                case 'd':
-                    num = va_arg(args, int);
-                    printf("%d", num);
-                    count += snprintf(NULL, 0, "%d", num);
-                    break;
-                case 'u':
-                    unum = va_arg(args, unsigned int);
-                    printf("%u", unum);
-                    count += snprintf(NULL, 0, "%u", unum);
-                    break;
-                case 'o':
-                    unum = va_arg(args, unsigned int);
-                    printf("%o", unum);
-                    count += snprintf(NULL, 0, "%o", unum);
-                    break;
-                case 'x':
-                    unum = va_arg(args, unsigned int);
-                    printf("%x", unum);
-                    count += snprintf(NULL, 0, "%x", unum);
-                    break;
-                case 'p':
-                    ptr = va_arg(args, void *);
-                    printf("%p", ptr);
-                    count += snprintf(NULL, 0, "%p", ptr);
-                    break;
-                case 'l':
-                    lnum = va_arg(args, long);
-                    printf("%ld", lnum);
-                    count += snprintf(NULL, 0, "%ld", lnum);
-                    break;
-                default:
-                    putchar('%');
-                    count++;
-                    putchar(*format);
-                    count++;
-                    break;
-            }
-        } else {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
+int main(void)
+{
+	int len;
 
-    va_end(args);
-    return count;
+	len = _printf("Let's try to printf a simple sentence.\n");
+	printf("Length:[%d]\n", len);
+	len = _printf("Character:[%c]\n", 'H');
+	printf("Length:[%d]\n", len);
+	len = _printf("String:[%s]\n", "I am a string !");
+	printf("Length:[%d]\n", len);
+	len = _printf("Percent:[%%]\n");
+	printf("Length:[%d]\n", len);
+	len = _printf("Negative:[%d]\n", -762534);
+	printf("Length:[%d]\n", len);
+	return 0;
 }
